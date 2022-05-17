e62ef9e492a021ea1fac98d0fe87b401d429df59
miguedeabreu <miguedeabreu@gmail.com>
2022-05-17 12:09:31 +0200

    Creando formulario para agregar contactos

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import Formulario from "./componentes/formulario";
 
+const App = () => {
+    return ( 
+        <Contenedor>
+            <Titulo>Lista de Contactos</Titulo>
+            <Formulario></Formulario>
+        </Contenedor>
+    );
+}
+
+const Contenedor = styled.div`
+    margin: 40px;
+    width: 90%;
+    max-width: 400px;
+    background: #fafafa;
+    padding: 40px;
+    border-radius: 5px;
+    text-align: center;
+`;
+
+const Titulo = styled.h2`
+    margin-bottom: 10px;
+`;
+ 
+export default App;
